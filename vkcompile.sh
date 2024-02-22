743332672625aa42983afa0395c7d61fdb6fd7c0
Mario Merlo <18537290+MrVideo@users.noreply.github.com>
2024-02-22 11:59:11 +0100

    Add script for compilation

---
+++ vkcompile.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+vulkan=~/VulkanSDK/1.3.275.0/macOS
+homebrew=/opt/homebrew
+
+g++ -std=c++17 -Iheaders -I$vulkan/include -I$homebrew/include -L$vulkan/lib -L$homebrew/lib -lglfw -lvulkan $1 -o $1.bin 
